5f4e2890c197b503a652c1262e966220b246ee85
WalkerDogW <WalkerDogW@outlook.com>
2019-11-25 16:26:57 +0800

    solve 175-176-177

---
+++ Medium/177_第N高的薪水.sql
@@ -0,0 +1,37 @@
+/*
+编写一个 SQL 查询，获取 Employee 表中第 n 高的薪水（Salary）。
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+例如上述 Employee 表，n = 2 时，应返回第二高的薪水 200。如果不存在第 n 高的薪水，那么查询应返回 null。
+
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+
+*/
+
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+
+drop procedure if exists getNthHighestSalary; 
+delimiter //
+create procedure  getNthHighestSalary(in n int)
+begin
+	set n = n-1;
+	select n;
+	select distinct(salary) from Employee order by salary desc limit n,1 ;
+end //
+delimiter ; 
+
+call getNthHighestSalary(2);
